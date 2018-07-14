90e4eb64617d2ef6235c68c43d5ac4ddc0312d18
Michelle Kim <pasolre95kim@gmail.com>
2018-07-14 21:40:10 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,11 +3,11 @@ def line(deli)
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-deli.each.with_index(1) do |person, i|
-  current_line << " #{i}. #{person}"
-  end
-    puts current_line
-  end
+   deli.each.with_index(1) do |person, i|
+     current_line << " #{i}. #{person}"
+   end
+   puts current_line
+ end
 end
 
 def take_a_number(deli, name)
